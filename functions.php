5832c7f9480d76c7b63236e807610812ca417b6c
Don Denton <don@happycollision.com>
2013-04-22 18:25:22 -0400

    Created Blog Post type and altered output of theme using only functions.php!

--- functions.php
+++ functions.php
@@ -1,6 +1,42 @@
 <?php
+locate_template('project_blog_post-type.php', true);
 
 add_action('wp_head','add_google_fonts');
 function add_google_fonts(){
 	echo "<link href='http://fonts.googleapis.com/css?family=Nothing+You+Could+Do' rel='stylesheet' type='text/css'>";
+}
+
+// Add class of 'project-blog' to body for styleing the blog page
+add_filter('body_class', 'projet_blog_body_class');
+function projet_blog_body_class($classes) {
+        $classes[] = 'project-blog';
+        return $classes;
+}
+
+/**
+ * Here are some customizations that change text output via the gettext filter.
+ * This was intended for translating themes to other languages, but why not
+ * use it for more customization?
+ *
+ * @link http://codex.wordpress.org/Plugin_API/Filter_Reference/gettext
+ *
+ */
+add_filter( 'gettext', 'happycol_change_excerpt_name', 20, 3 );
+function happycol_change_excerpt_name( $translated_text, $text, $domain ) {
+
+    if( is_post_type_archive('blog') ) {
+
+        switch ( $translated_text ) {
+
+            case 'Archives' :
+
+                $translated_text = 'Project Blog';
+                break;
+
+
+        }
+
+    }
+
+    return $translated_text;
 }
---
+++ project_blog_post-type.php
@@ -0,0 +1,36 @@
+<?php
+function codex_custom_init() {
+  $labels = array(
+    'name' => 'Blog',
+    'singular_name' => 'Blog Post',
+    'add_new' => 'Add New',
+    'add_new_item' => 'Add New Blog Post',
+    'edit_item' => 'Edit Blog Post',
+    'new_item' => 'New Blog Post',
+    'all_items' => 'All Blog Posts',
+    'view_item' => 'View Blog Post',
+    'search_items' => 'Search Blog Posts',
+    'not_found' =>  'No blog posts found',
+    'not_found_in_trash' => 'No blog posts found in Trash', 
+    'parent_item_colon' => '',
+    'menu_name' => 'Blog'
+  );
+
+  $args = array(
+    'labels' => $labels,
+    'public' => true,
+    'publicly_queryable' => true,
+    'show_ui' => true, 
+    'show_in_menu' => true, 
+    'query_var' => true,
+    'rewrite' => array( 'slug' => 'blog' ),
+    'capability_type' => 'post',
+    'has_archive' => true, 
+    'hierarchical' => false,
+    'menu_position' => null,
+    'supports' => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' )
+  ); 
+
+  register_post_type( 'blog', $args );
+}
+add_action( 'init', 'codex_custom_init' );
