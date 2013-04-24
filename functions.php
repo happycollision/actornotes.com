88ad6dc93ae8af9b91c65518c891dd9e6fe4e6b4
Don Denton <don@happycollision.com>
2013-04-23 20:22:08 -0400

    Added images and links for Facebook/Open Graph

--- functions.php
+++ functions.php
@@ -3,18 +3,39 @@ locate_template('project_blog_post-type.php', true);
 
 add_action('wp_head','add_google_fonts');
 function add_google_fonts(){
-	echo "<link href='http://fonts.googleapis.com/css?family=Nothing+You+Could+Do' rel='stylesheet' type='text/css'>";
+	echo "<link href='http://fonts.googleapis.com/css?family=Nothing+You+Could+Do' rel='stylesheet' type='text/css'>" . "\n";
 }
 
 add_action('wp_head','add_favicons');
 function add_favicons(){
-	echo '<link rel="shortcut icon" href="'.get_stylesheet_directory_uri() . '/favicon.ico'.'">';
+	echo '<link rel="shortcut icon" href="'.get_stylesheet_directory_uri() . '/favicon.ico'.'">' . "\n";
 	
-	echo '<link rel="apple-touch-icon" href="'.get_stylesheet_directory_uri() . '/apple-touch-icon-precomposed.png'.'" />';
-	echo '<link rel="apple-touch-icon" sizes="72x72" href="'.get_stylesheet_directory_uri() . '/apple-touch-icon-72x72-precomposed.png'.'" />';
-	echo '<link rel="apple-touch-icon" sizes="114x114" href="'.get_stylesheet_directory_uri() . '/apple-touch-icon-114x114-precomposed.png'.'" />';
-	//echo '<link rel="apple-touch-icon" sizes="144x144" href="'.get_stylesheet_directory_uri() . '/apple-touch-icon-precomposed.png'.'" />';
+	echo '<link rel="apple-touch-icon" href="'.get_stylesheet_directory_uri() . '/apple-touch-icon-precomposed.png'.'" />' . "\n";
+	echo '<link rel="apple-touch-icon" sizes="72x72" href="'.get_stylesheet_directory_uri() . '/apple-touch-icon-72x72-precomposed.png'.'" />' . "\n";
+	echo '<link rel="apple-touch-icon" sizes="114x114" href="'.get_stylesheet_directory_uri() . '/apple-touch-icon-114x114-precomposed.png'.'" />' . "\n";
+	//echo '<link rel="apple-touch-icon" sizes="144x144" href="'.get_stylesheet_directory_uri() . '/apple-touch-icon-precomposed.png'.'" />' . "\n";
 }
+
+function hc_single_id(){
+	global $wp_query;
+	$post_id = $wp_query->post->ID;
+	return $post_id;
+}
+
+add_action('wp_head','open_graph_stuff');
+function open_graph_stuff(){
+	echo '<meta property="og:image" content="'.get_stylesheet_directory_uri() . '/i/actor-notes-image.jpg'.'" />' . "\n";
+	echo '<meta property="og:image" content="'.get_stylesheet_directory_uri() . '/i/actor-notes-image2.jpg'.'" />' . "\n";
+}
+
+add_action('wp_head','hc_meta_info');
+function hc_meta_info(){
+	echo '<meta name="Collaborative acting thoughts. Read journal entries from other actors and contribute your own. Let\'s all have an epiphany together!" />' . "\n";
+	echo '<link rel="image_src" href="'.get_stylesheet_directory_uri() . '/i/actor-notes-image.jpg'.'" />' . "\n";
+	echo '<link rel="image_src" href="'.get_stylesheet_directory_uri() . '/i/actor-notes-image2.jpg'.'" />' . "\n";
+}
+
+
 
 // Add class of 'project-blog' to body for styleing the blog page
 add_filter('body_class', 'projet_blog_body_class');
