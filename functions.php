7eb1f635d2638d1d03bdba0ae547412d7ab6b807
Don Denton <don@happycollision.com>
2013-04-25 03:10:03 -0400

    Added pseudo shortlink ability for custom post types

--- functions.php
+++ functions.php
@@ -150,3 +150,39 @@ function dddn_process($id) {
 }
 
 add_action('save_post', 'dddn_process');
+
+
+/*
+Add get shortlink to all publicly queriable types
+*/
+if( !function_exists( 'my_theme_cpt_shortlinks' ) ) {
+	/**
+	 * Allow shortlinks to be retrieved for pages and custom post types
+	 */
+	function my_theme_cpt_shortlinks( $shortlink, $id, $context, $allow_slugs=true ) {
+		/**
+		 * If query is the context, we probably shouldn't do anything
+		 */
+		if( 'query' == $context )
+			return $shortlink;
+
+		$post = get_post( $id );
+		$post_id = $post->ID;
+
+		/**
+		 * If this is a standard post, return the shortlink that was already built
+		 */
+		if( 'post' == $post->post_type )
+			return $shortlink;
+
+		/**
+		 * Retrieve the array of publicly_queryable, non-built-in post types
+		 */
+		$post_types = get_post_types( array( '_builtin' => false, 'publicly_queryable' => true ) );
+		if( in_array( $post->post_type, $post_types ) || 'page' == $post->post_type )
+			$shortlink = home_url('?p=' . $post->ID);
+
+		return $shortlink;
+	}
+}
+add_filter( 'get_shortlink', 'my_theme_cpt_shortlinks', 10, 4 );
